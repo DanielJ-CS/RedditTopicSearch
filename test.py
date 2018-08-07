746fbc65e87f73d389114888eb4786276653da7b
Systema7ic <jdanielslss123@gmail.com>
2018-08-07 14:40:24 -0700

    Added Main()

--- test.py
+++ test.py
@@ -1,30 +1,35 @@
 import praw
 
+
 reddit = praw.Reddit(user_agent='pikv',
-                      client_id='ixIkjWMxgYLbxA', client_secret='0KqTxCHUGGUfktFO-qIxqMbwVmE')
-topic = 'hifiman'
-top_subreddits = []
-amountofSubreddits = 3
-amountofComments = 3
-amountinSearch = 1
+                     client_id='ixIkjWMxgYLbxA', client_secret='0KqTxCHUGGUfktFO-qIxqMbwVmE')
 
-for array_subreddit in reddit.subreddits.search(topic):
-    top_subreddits.append(array_subreddit.display_name)
+def main():
+    topic = 'hifiman'
+    top_subreddits = []
+    amountofSubreddits = 3
+    amountofComments = 3
+    amountinSearch = 1
 
-for subreddit in top_subreddits[:amountofSubreddits]:
-    for submission in reddit.subreddit(subreddit).search(topic,limit=amountinSearch):
-        count = amountofComments
-        if not submission.stickied:
-            print('UPVOTES: ', submission.ups)
-            print('SUBREDDIT: ', subreddit)
-            print('POST: ', submission.title)
+    for array_subreddit in reddit.subreddits.search(topic):
+        top_subreddits.append(array_subreddit.display_name)
 
-            submission.comments.replace_more(limit=None)
-            for comment in submission.comments.list():
-                if not comment.stickied:
-                    if count > 0:
-                        print('UPVOTES: ', comment.ups)
-                        print('REPLY: ', comment.body)
-                        count = count - 1
+    for subreddit in top_subreddits[:amountofSubreddits]:
+        for submission in reddit.subreddit(subreddit).search(topic,limit=amountinSearch):
+            count = amountofComments
+            if not submission.stickied:
+                print('UPVOTES: ', submission.ups)
+                print('SUBREDDIT: ', subreddit)
+                print('POST: ', submission.title)
+
+                submission.comments.replace_more(limit=None)
+                for comment in submission.comments.list():
+                    if not comment.stickied:
+                        if count > 0:
+                            print('UPVOTES: ', comment.ups)
+                            print('REPLY: ', comment.body)
+                            count = count - 1
 
 
+if __name__ == "__main__" :
+    main()
