a7735322b89dc6615c9e66efd112f6b217627246
Systema7ic <jdanielslss123@gmail.com>
2018-08-17 11:31:45 -0700

    Added plotting through matplotlib, fixed stopwords to fully include punctuation, changed unicode to string in dictionary

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 import praw
-
+import operator
+import numpy as np
+import string
+import unicodedata
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
 
 reddit = praw.Reddit(user_agent='pikv',
                      client_id='ixIkjWMxgYLbxA', client_secret='0KqTxCHUGGUfktFO-qIxqMbwVmE')
+
+dictionary = dict()
+commentArray = []
 
 def reddit_search(keyword,numberofSubreddits,numberofThreads,numberofComments):
     topic = keyword
@@ -28,8 +39,36 @@ def reddit_search(keyword,numberofSubreddits,numberofThreads,numberofComments):
                         if count > 0:
                             print('UPVOTES: ', comment.ups)
                             print('REPLY: ', comment.body)
+                            commentArray.append(comment.body)
                             count = count - 1
 
+def store_Comments(commentArray):
+    stop_words = set(stopwords.words("english"))
+    for comment in commentArray:
+        commentToken = word_tokenize(comment.lower())
+        filteredArray = [w for w in commentToken if not w in stop_words]
+        for word in filteredArray:
+            if word not in string.punctuation:
+                word = unicodedata.normalize('NFKD',word).encode('ascii', 'ignore')
+                if word != '':
+                    dictionary[word] = dictionary.get(word,0) + 1
+
+# A tolerance value on the number of words is needed so that the plot does not overpopulate
+def plot_dict(numberOfWords):
+    fullList = sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True)
+    sortedList = fullList[:numberOfWords]
+    print(sortedList)
+    x,y = zip(*sortedList)
+    xs = np.arange(len(x))
+    width = 1
+
+    plt.bar(xs,y,width,align='center')
+    plt.xticks(xs,x)
+    plt.xticks(rotation=90)
+    plt.yticks(y)
+    plt.savefig('netscore.png')
+
+    plt.show()
 
 def main():
     topic = str(raw_input('What would you like to find reddit posts about: '))
@@ -38,6 +77,8 @@ def main():
     comments = input('How many comments would you like to see: ')
 
     reddit_search(topic,subreddits,threads,comments)
+    store_Comments(commentArray)
+    plot_dict(10)
 
 if __name__ == "__main__" :
     main()
